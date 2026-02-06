f4bb5ac91052df90a9aa49e17a84b2b1bee565d2
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2026-02-06 17:49:51 +0000

    did alot

--- Heap.java
+++ Heap.java
@@ -1,1 +1,67 @@
 
+public class Heap {
+
+    //the actual storage structure for your heap
+
+    private int[] arr;
+
+    
+
+    //constructor for your heap
+
+    //feel free to make one that takes in an array if you prefer for your testing purposes.
+
+    //note that we will not be inserting more than 100 elements into our array so you need not worry about re-sizing
+
+    //the array
+
+    public Heap() {
+
+    arr = new int[100];
+
+    }
+
+    
+
+    
+
+    //create this function to add elements to your heap
+
+    //all heap properties must be preserved
+
+    // 5 points
+
+    public void add(int toAdd) {
+
+    
+
+    }
+
+    
+
+    //remove the largest element of the heap (the root) and re-heapafy
+
+    //5 points
+
+    public void removeMax() {}
+
+    
+
+    //this should check and alter the tree after an item is inserted
+
+    //3 points
+
+    private void siftUp(int index) {}
+
+    
+
+    //this should check and alter the tree after an item is deleted.
+
+    //3 points
+
+    private void siftDown(int index) {}
+
+    
+
+    //4 points for syntax conventions.
+}
