0dad35a0a7c09446e2e8dd5351bc788c4f296abb
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2026-02-10 17:42:00 +0000

    Fully completed MaxHeap

--- Main.java
+++ Main.java
@@ -5,11 +5,11 @@ public class Main
     public static void main(String[] args)
     {
         MaxHeap heap = new MaxHeap();
-        heap.add(50);
         heap.add(100);
-        heap.add(25);
-        heap.add(75);
         heap.add(200);
+        heap.add(50);
+        heap.add(25);
+        heap.add(125);
         heap.add(300);
         System.out.println(heap.stringFormat());
         heap.removeMax();
